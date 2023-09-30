66e3f5270b82caf0efa6d2e9c9f329ff2c68cddb
ayaka-tajiri <ayaka0727soudaiduou39@gmain.com>
2023-09-30 23:04:44 +0900

    add firestore

---
+++ src/FirebaseConfig.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB98oIlLyALmcZRzsDG6SJjyvhFMjU-pAw",
+    authDomain: "nostal-profile.firebaseapp.com",
+    projectId: "nostal-profile",
+    storageBucket: "nostal-profile.appspot.com",
+    messagingSenderId: "606515552991",
+    appId: "1:606515552991:web:ad2803d1ac63041caabf6a",
+    measurementId: "G-3S4K6BHD41"
+};
+
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,32 @@
 <script setup lang="ts">
 console.log("About");
+
+import { ref, onMounted } from 'vue'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../FirebaseConfig'
+
+const data = ref()
+const isLoading = ref(false)
+const error = ref()
+
+const getNostalProfile = async () => {
+  console.log("getNostalProfile")
+  isLoading.value = true
+  try {
+    const c = collection(db, 'nostal-profiles');
+    const s = await getDocs(c);
+    data.value = s.docs.map(doc => doc.data());
+    console.log(data.value)
+  } catch (e) {
+    error.value = e
+  } finally {
+    isLoading.value = false
+  }
+}
+
+onMounted(() => {
+  getNostalProfile()
+})
 </script>
 
 <template>
