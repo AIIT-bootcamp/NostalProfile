b31d67499a167c0700cde4f7c2b6c10b90a3719f
jabelic <hibiki990523@gmail.com>
2023-09-30 17:55:45 +0900

    feat: 🎸 vue-routerの設定

---
+++ src/pages/home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+console.log("hello");
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Home page content</p>
+  </div>
+</template>
+
+<style scoped>
+* {
+  color: red;
+}
+</style>
