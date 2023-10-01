3105cc56f02c086cb2531ab5bdb004782a91d7cd
ktr <ktr.w1011@gmail.com>
2023-10-01 11:27:10 +0900

    add css home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -19,12 +19,95 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
+  <div class="center-container">
+    <div class="title">
+      <img
+        class="image-inline"
+        v-for="n in 3"
+        :key="n"
+        src="src/assets/images/kirakira_star.gif"
+        alt="star"
+      >
+      メニュー
+      <img
+        class="image-inline"
+        v-for="n in 3"
+        :key="n"
+        src="src/assets/images/kirakira_star.gif"
+        alt="star"
+      >
+    </div>
+    <div class="title">
+      <img class="image-inline" src="src/assets/images/shooting_star.gif">
+        はじめての方
+      <img class="image-inline" src="src/assets/images/shooting_star.gif">
+    </div>
     <router-link to="/new">プロフを作る</router-link>
+    <div class="title">
+      <div class="marquee">
+        <div class="marquee-content">
+          ＼＼＼ ﾅ ｶ ﾏ をさがす ／／／
+        </div>
+      </div>
+    </div>
+    <div class="title">
+      <img
+        class="image-inline"
+        v-for="n in 8"
+        :key="n"
+        src="src/assets/images/lovely_heart.gif"
+        alt="star"
+      >
+    </div>
     <div v-for="item in dataAry">
       <router-link :to="`/posts/${item.id}`">{{ item.username }}</router-link>
+    </div>
+    <div class="title">
+      <img
+        class="image-inline"
+        v-for="n in 8"
+        :key="n"
+        src="src/assets/images/lovely_heart.gif"
+        alt="star"
+      >
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .center-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #ff66b2;
+  }
+  .title {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+  .image-inline {
+    display: inline-block;
+    vertical-align: middle;
+  }
+
+  .marquee {
+    overflow: hidden;
+    white-space: nowrap;
+  }
+  
+  .marquee-content {
+    display: inline-block;
+    padding-left: 100%;
+    animation: marquee 5s linear infinite;
+  }
+
+  @keyframes marquee {
+    0% {
+      transform: translate(0, 0);
+    }
+    100% {
+      transform: translate(-100%, 0);
+    }
+  }
+</style>
